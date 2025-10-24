3c7345577323a632a850525a61ad10c17d989a1c
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-25 03:11:50 +0530

    Adding Transactional Annotation

--- journalApp/src/main/java/com/edigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/edigest/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.edigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +21,18 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        journalEntry.setDate(LocalDateTime.now());
-        User user = userService.findByUserName(userName);
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            User user = userService.findByUserName(userName);
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        } catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while saving the entry", e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry){
